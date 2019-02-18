d9b8d7c4896b59ffb8afe4477ef54d1e783b0b37
David Atchley <david.m.atchley@gmail.com>
2019-02-18 12:36:34 -0600

    testing build/deploy pipeline

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, '../build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -16,6 +18,10 @@ app.post("/api/world", (req, res) => {
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`
   );
+});
+// Serve Client Application
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
