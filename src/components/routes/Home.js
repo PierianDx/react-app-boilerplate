3091d8c532491cb303860c074ac6c984a2e6d9a3
Ole Andreas Birkedal <oleandreasbirkedal@Oles-MacBook-Pro.local>
2019-02-20 11:55:41 -0600

    Test

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -42,6 +42,7 @@ class Home extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p>No more spreadsheets!</p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
